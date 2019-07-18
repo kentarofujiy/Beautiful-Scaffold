a2a737d669f9f0a01e2f5f7dcf74cc6089ad4ab8
Kentaro Fujiy <kentaro@manacadigital.com.br>
2019-07-18 02:06:29 +0000

    supergit

--- lib/generators/beautiful_migration_generator.rb
+++ lib/generators/beautiful_migration_generator.rb
@@ -48,7 +48,7 @@ class BeautifulMigrationGenerator < Rails::Generators::Base
       end
 
       inject_into_file("app/models/#{model}.rb", ":#{a},", :after => "def self.permitted_attributes\n    return ")
-      if not options[:withavatar].blank? then
+      unless options[:withavatar].blank? then
         inject_into_file("app/models/#{model}.rb", ":avatar,", :after => "def self.permitted_attributes\n    return ")
       end
     }
